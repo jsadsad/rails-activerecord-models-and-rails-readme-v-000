5a94e4d7e8275c3db5b9b64d23229a3950b0d46f
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-06-27 00:12:22 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Post do
-  if 'can be created' do
+  it 'can be created' do
     post = Post.create!(:title: "My Title", description: "Test description")
     expect(post).to be_valid
   end
